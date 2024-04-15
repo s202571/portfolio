db22c8e0cf054e1ab662da14f1fa1302fc2b02be
ej <s202571@naver.com>
2024-04-15 18:49:15 +0900

    완료

--- script/script.js
+++ script/script.js
@@ -169,8 +169,3 @@ window.addEventListener('mousemove', (e)=>{
 })
 
 const result_btn = document.querySelectorAll('.result_btn a')
-
-console.log(result_btn)
-result_btn[7].addEventListener('click',()=>{
-    alert('현재 제작중 입니다.')
-})
